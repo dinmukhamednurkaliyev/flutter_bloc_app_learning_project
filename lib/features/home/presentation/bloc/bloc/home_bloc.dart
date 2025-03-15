16df4d266e024d4a40d1ba5d4cd4975a0da24de4
Dinmukhamed Nurkaliyev <dinmukhamed.nurkaliyev@gmail.com>
2025-03-16 01:14:20 +0500

    feat(home): Implement initial event handling in HomeBloc

--- lib/features/home/presentation/bloc/bloc/home_bloc.dart
+++ lib/features/home/presentation/bloc/bloc/home_bloc.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -6,8 +8,33 @@ part 'home_state.dart';
 
 class HomeBloc extends Bloc<HomeEvent, HomeState> {
   HomeBloc() : super(HomeInitialState()) {
-    on<HomeEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+    on<HomeWishlistButtonNavigateClickedEvent>(
+      homeWishlistButtonNavigateClickedEvent,
+    );
+    on<HomeCartButtonNavigateClickedEvent>(homeCartButtonNavigateClickedEvent);
+    on<HomeProductWishlistButtonClickedEvent>(
+      homeProductWishlistButtonClickedEvent,
+    );
+    on<HomeProductCartButtonClickedEvent>(homeProductCartButtonClickedEvent);
   }
+
+  FutureOr<void> homeWishlistButtonNavigateClickedEvent(
+    HomeWishlistButtonNavigateClickedEvent event,
+    Emitter<HomeState> emit,
+  ) {}
+
+  FutureOr<void> homeCartButtonNavigateClickedEvent(
+    HomeCartButtonNavigateClickedEvent event,
+    Emitter<HomeState> emit,
+  ) {}
+
+  FutureOr<void> homeProductWishlistButtonClickedEvent(
+    HomeProductWishlistButtonClickedEvent event,
+    Emitter<HomeState> emit,
+  ) {}
+
+  FutureOr<void> homeProductCartButtonClickedEvent(
+    HomeProductCartButtonClickedEvent event,
+    Emitter<HomeState> emit,
+  ) {}
 }
