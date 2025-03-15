0d45479a9db976c0457fa112e0b09e9ebe72c166
Dinmukhamed Nurkaliyev <dinmukhamed.nurkaliyev@gmail.com>
2025-03-16 01:41:24 +0500

    feat(home): Enable navigation to Cart and Wishlist from Home screen

--- lib/features/home/presentation/pages/home_page.dart
+++ lib/features/home/presentation/pages/home_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_bloc_app_learning_project/features/cart/presentation/pages/cart_page.dart';
 import 'package:flutter_bloc_app_learning_project/features/home/presentation/bloc/bloc/home_bloc.dart';
+import 'package:flutter_bloc_app_learning_project/features/wishlist/presentation/pages/wishlist_page.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -15,31 +17,38 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return BlocConsumer<HomeBloc, HomeState>(
       bloc: bloc,
-      listenWhen: (previous, current) {
-        return true;
-      },
-      buildWhen: (previous, current) {
-        return true;
-      },
+      listenWhen: (previous, current) => current is HomeActionState,
+      buildWhen: (previous, current) => current is! HomeActionState,
       listener: (context, state) {
-        // TODO: implement listener
+        if (state is HomeNavigateToCartPageActionState) {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => CartPage()),
+          );
+        } else if (state is HomeNavigateToWishlistPageActionState) {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => WishlistPage()),
+          );
+        }
       },
       builder: (context, state) {
         return Scaffold(
           appBar: AppBar(
+            backgroundColor: Colors.teal,
             title: Text('Grocery App'),
             actions: [
               IconButton(
                 onPressed: () {
                   bloc.add(HomeWishlistButtonNavigateClickedEvent());
                 },
-                icon: Icon(Icons.favorite),
+                icon: Icon(Icons.favorite_outline),
               ),
               IconButton(
                 onPressed: () {
                   bloc.add(HomeCartButtonNavigateClickedEvent());
                 },
-                icon: Icon(Icons.shopping_bag),
+                icon: Icon(Icons.shopping_bag_outlined),
               ),
             ],
           ),
