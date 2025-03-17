9ac0d60b3977da1ff738eff6adc8a082f1606c3a
Dinmukhamed Nurkaliyev <dinmukhamed.nurkaliyev@gmail.com>
2025-03-18 03:15:55 +0500

    feat(home): Add ProductTileWidget for displaying product items

---
+++ lib/features/home/presentation/widgets/product_tile_widget.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc_app_learning_project/features/home/data/models/product_model.dart';
+
+class ProductTileWidget extends StatelessWidget {
+  final ProductModel product;
+  const ProductTileWidget({super.key, required this.product});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
