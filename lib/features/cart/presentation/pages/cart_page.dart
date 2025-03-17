1fe8967a6fb8709198288119d343fc21bd5cfd1a
Dinmukhamed Nurkaliyev <dinmukhamed.nurkaliyev@gmail.com>
2025-03-18 04:46:25 +0500

    feat(cart): Setup basic BLoC for cart page

--- lib/features/cart/presentation/pages/cart_page.dart
+++ lib/features/cart/presentation/pages/cart_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc_app_learning_project/features/cart/presentation/bloc/bloc/cart_bloc.dart';
 
 class CartPage extends StatefulWidget {
   const CartPage({super.key});
@@ -8,6 +9,7 @@ class CartPage extends StatefulWidget {
 }
 
 class _CartPageState extends State<CartPage> {
+  final CartBloc bloc = CartBloc();
   @override
   Widget build(BuildContext context) {
     return Scaffold();
